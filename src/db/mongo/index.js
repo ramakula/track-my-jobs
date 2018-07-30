5494cd220f920b776c6d1bf549ba862a71df15f8
Samuel Cha <sunchipssam@gmail.com>
2018-07-30 05:07:59 -0700

    Added PROD for mongo

--- src/db/mongo/index.js
+++ src/db/mongo/index.js
@@ -1,21 +1,30 @@
 const MONGODB = JSON.parse(process.env.MONGODB);
 
 const mongoose = require('../../server.dependencies').mongoose;
+switch (process.env.CURR_ENV) {
+  case 'DEV':
+    return mongoose.connect(
+      MONGODB.DEV.path,
+      MONGODB.DEV.options,
+      err => {
+        if (err) {
+          console.log(`Error connecting to ${MONGODB.DEV.path}: ${err}`);
+          return;
+        }
 
-if (JSON.parse(process.env.DEV)) {
-  mongoose.connect(
-    MONGODB.DEV.path,
-    MONGODB.DEV.creds,
-    err => {
-      if (err) {
-        console.log(`Error connecting to ${MONGODB.DEV.path}`);
-        return;
-      }
+        console.log(`Connected to ${MONGODB.DEV.path}`);
+      },
+    );
 
-      console.log(`Connected to ${MONGODB.DEV.path}`);
-      return;
-    },
-  );
+  case 'PROD':
+    return mongoose.connect(
+      MONGODB.PROD.path,
+      MONGODB.PROD.options,
+      err => {
+        if (err) console.log(`Error connecting to db: ${err}`);
+      },
+    );
+
+  default:
+    if (JSON.parse(process.env.DEBUG)) console.log(`curr env not specified`);
 }
-
-// add MLAB connect here and to .env
