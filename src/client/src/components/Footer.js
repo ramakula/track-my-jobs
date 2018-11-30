de9d86969969ac5e06898643f93e89b390d16ba7
Samuel Cha <sunchipssam@gmail.com>
2018-11-29 18:15:47 -0600

    update footer to use less

--- src/client/src/components/Footer.js
+++ src/client/src/components/Footer.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styledLinkHelper from './meta/StyledLink/StyledLink.helper';
 
 export default _ => (
   <div className="footer">
     <div className="links">
-      <div className="links__item">
-        {styledLinkHelper('/about', 'about', '#141414')}
-      </div>
+      <Link className="links__item" to="/about">
+        about
+      </Link>
 
-      <div className="links__item">
-        {styledLinkHelper('/support', 'support', '#141414')}
-      </div>
+      <Link className="links__item" to="/support">
+        support
+      </Link>
 
-      <div className="links__item">
-        {styledLinkHelper('/donate', 'donate', '#141414')}
-      </div>
+      <Link className="links__item" to="/donate">
+        donate
+      </Link>
     </div>
 
     <div className="copyright">&copy;2018 All Rights Reserved</div>
